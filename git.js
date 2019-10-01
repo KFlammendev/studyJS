59a9a49bcb52d9d678408f3979dcaf64bb4152cf
Flammen <flammen.dev@gmail.com>
2019-10-01 07:38:54 +0300

    initial commit

---
+++ git.js
@@ -0,0 +1,14 @@
+More info on git how to 
+
+//to start working with GIT, write this in terminal
+git init
+git config --local user.name "..."
+git config --local user.email flammen.dev@gmail.com 
+
+git status 
+// to check it out
+
+git add -A
+//"A" means all, its added all files in a curent directory
+
+git commit -a -m"name of control point"
---
+++ variables.js
@@ -0,0 +1,14 @@
+console.log(leftBoarderWidth);
+// Переменные \ variables
+var leftBoarderWidth = 1; /* Переменная Имя = значение/ Если объявлена, то она видна везде */
+let second = 2;
+const pi = 3.14;
+
+/* Data types */
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
